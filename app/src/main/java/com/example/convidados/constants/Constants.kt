f83229e33d95ec9534961fc362d5280e6b6efe26
Thooor7 <heitor.dev7@gmail.com>
2024-02-19 19:53:39 -0300

    banco de dados - atualizacao, remocao e listagem sem filtros

--- app/src/main/java/com/example/convidados/constants/Constants.kt
+++ app/src/main/java/com/example/convidados/constants/Constants.kt
@@ -3,7 +3,7 @@ package com.example.convidados.constants
 class Constants private constructor(){
 
     companion object GUEST{
-        const val TABLE_NAME = "Guest"
+        const val GUEST = "Guest"
 
         object COLUMNS{
             const val ID = "id"
--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -3,6 +3,7 @@ package com.example.convidados.repository
 import android.content.ContentValues
 import android.content.Context
 import com.example.convidados.constants.Constants
+import com.example.convidados.constants.Constants.GUEST.COLUMNS.ID
 import com.example.convidados.constants.Constants.GUEST.COLUMNS.NAME
 import com.example.convidados.constants.Constants.GUEST.COLUMNS.PRESENCE
 import com.example.convidados.model.GuestModel
@@ -32,10 +33,76 @@ class GuestRepository private constructor(context: Context) {
             values.put(PRESENCE, presence)
             values.put(NAME, guest.name)
 
-            db.insert(Constants.TABLE_NAME, null, values)
+            db.insert(Constants.GUEST, null, values)
             true
         } catch (e: Exception) {
             false
         }
+    }
+
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val presence = if (guest.presence) 1 else 0
+            val values = ContentValues()
+            val selection = "id = ?"
+            val args = arrayOf(guest.id.toString())
+
+            values.put(PRESENCE, presence)
+            values.put(NAME, guest.name)
+
+            db.update(Constants.GUEST, values, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun delete(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selection = "id = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.delete(Constants.GUEST, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun  getAll(): List<GuestModel> {
+
+        val list = mutableListOf<GuestModel>()
+
+        try {
+            val db = guestDataBase.readableDatabase
+
+            val selection = arrayOf(
+                ID,
+                NAME,
+                PRESENCE
+            )
+
+            val cursor = db.query(Constants.GUEST, selection, null,
+                null, null, null, null)
+
+            if (cursor != null && cursor.count > 0){
+                while (cursor.moveToNext()){
+                    val id = cursor.getInt(cursor.getColumnIndex(ID))
+                    val name = cursor.getString(cursor.getColumnIndex(NAME))
+                    val presence = cursor.getInt(cursor.getColumnIndex(PRESENCE))
+
+                    list.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+            cursor.close()
+        } catch (e: Exception){
+            return list
+        }
+        return list
     }
 }
